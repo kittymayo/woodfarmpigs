39f618d1f4789c729454ad88eb963eccf36edb64
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-03-28 11:19:31 +0000

    initial rails build

--- woodfarm/config/routes.rb
+++ woodfarm/config/routes.rb
@@ -1,43 +1,10 @@
 Rails.application.routes.draw do
-  get 'baskets/new'
-  get 'baskets/create'
-  get 'baskets/update'
-  get 'baskets/edit'
-  get 'baskets/destroy'
-  get 'baskets/show'
-  get 'basket_items/new'
-  get 'basket_items/create'
-  get 'basket_items/update'
-  get 'basket_items/edit'
-  get 'basket_items/destroy'
-  get 'basket_items/show'
-  get 'basket_items/index'
-  get 'orders/new'
-  get 'orders/create'
-  get 'orders/update'
-  get 'orders/edit'
-  get 'orders/destroy'
-  get 'orders/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/update'
-  get 'products/edit'
-  get 'products/destroy'
-  get 'products/index'
-  get 'products/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/update'
-  get 'users/edit'
-  get 'users/destroy'
-  get 'users/index'
-  get 'users/show'
   root to: 'pages#home'
 
   resources :products do
-    resources :basket_items, only: [:new, :create, :destroy, :edit, :update] do
-      resources :baskets, only: [:new, :create, :edit, :update] do
-        resources :orders, only: [:new, :create, :edit, :update, :destroy]
+    resources :basket_items do
+      resources :baskets do
+        resources :orders
       end
     end
   end
